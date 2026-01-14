1310cba63cdb9a821ae2b9770e2ee3e7b98f900e
uanela <uanelaluiswayne@gmail.com>
2026-01-14 15:04:09 +0200

    first commit

---
+++ init.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_user_command("CounBuffs",
+    function()
+      local ids = vim.api.nvim_list_bufs()
+      print("Opened buffers:\n")
+      for _, id in pairs(ids) do
+        if vim.api.nvim_buf_is_loaded(id) then
+          local name = vim.api.nvim_buf_get_name(id)
+          print(id .. ". " .. name)
+        end
+      end
+    end, {}
+  )
+end
+
+M.setup()
+
+return M
